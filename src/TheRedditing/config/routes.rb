7efb8dc57d663beec1d168013f2e9f354b1a9774
Will Hosford <hosford.will@gmail.com>
2017-06-17 22:14:00 -0700

    finish user sign up

--- src/TheRedditing/config/routes.rb
+++ src/TheRedditing/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 	get  '/about',    to: 'static_pages#about'
   	get  '/faq',   to: 'static_pages#faq'
   	get  '/signup',  to: 'users#new'
+  	post '/signup',  to: 'users#create'
+  	resources :users
 end
