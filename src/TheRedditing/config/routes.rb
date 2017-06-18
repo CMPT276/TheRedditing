d27fa82c73851bf57af1445a38df3c279e8cce3f
Will Hosford <hosford.will@gmail.com>
2017-06-18 16:37:14 -0700

    Add account activation

--- src/TheRedditing/config/routes.rb
+++ src/TheRedditing/config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-	root 'static_pages#home'
-	get  '/about',    to: 'static_pages#about'
-  	get  '/faq',   to: 'static_pages#faq'
-  	get  '/signup',  to: 'users#new'
-  	post '/signup',  to: 'users#create'
-  	get    '/login',   to: 'sessions#new'
-  	post   '/login',   to: 'sessions#create'
-  	delete '/logout',  to: 'sessions#destroy'
-  	resources :users
+  root   'static_pages#home'
+  get    '/faq',    to: 'static_pages#faq'
+  get    '/about',   to: 'static_pages#about'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
+  resources :account_activations, only: [:edit]
 end
