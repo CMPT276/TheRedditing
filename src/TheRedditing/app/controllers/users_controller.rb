7618527efbb29bf4eb2f2141a0d77b573ea5fc62
Will Hosford <hosford.will@gmail.com>
2017-06-17 23:19:09 -0700

    added login

--- src/TheRedditing/app/controllers/users_controller.rb
+++ src/TheRedditing/app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 	def create
     	@user = User.new(user_params)
     	if @user.save
-    		flash[:success] = "Welcome to The Redditing!"
+          log_in @user
+    		  flash[:success] = "Welcome to The Redditing!"
       		redirect_to @user
     	else
       		render 'new'
