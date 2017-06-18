a82c2341ca01b2748c1bcfc8983b1eba9f756174
Will Hosford <hosford.will@gmail.com>
2017-06-18 16:57:51 -0700

    minor changes

--- src/TheRedditing/db/seeds.rb
+++ src/TheRedditing/db/seeds.rb
@@ -7,22 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Will Hosford",
+             email: "hosford.will@gmail.com",
+             password:              "Wst1tGPE4s",
+             password_confirmation: "Wst1tGPE4s",
              admin:     true,
              activated: true,
              activated_at: Time.zone.now)
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
-end
