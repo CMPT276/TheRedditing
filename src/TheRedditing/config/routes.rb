c1715e1038a2fc74620bc4d67c0c6829487ac92c
kade <kade_chen@hotmail.com>
2017-07-08 21:26:08 -0700

    added preference page

--- src/TheRedditing/config/routes.rb
+++ src/TheRedditing/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
+  get    '/preference',    to: 'static_pages#preference'
   get    '/faq',    to: 'static_pages#faq'
   get    '/about',   to: 'static_pages#about'
   get    '/signup',  to: 'users#new'
